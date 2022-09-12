28c735f0e4d522cef8ec141d14cc94bd5058d340
seregatheone <sergey20030707@gmail.com>
2022-09-12 19:23:28 +0300

    splashscreen

---
+++ app/src/main/java/com/patproject/test/battleship/splashscreen/SplashscreenActivity.kt
@@ -0,0 +1,17 @@
+package com.patproject.test.battleship.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.patproject.test.battleship.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
