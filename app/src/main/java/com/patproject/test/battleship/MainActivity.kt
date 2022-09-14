ab2fc296cf9ebab810bc70b2df5cb720dee3f55b
seregatheone <sergey20030707@gmail.com>
2022-09-14 14:25:03 +0300

    launcher icon changed

--- app/src/main/java/com/patproject/test/battleship/MainActivity.kt
+++ app/src/main/java/com/patproject/test/battleship/MainActivity.kt
@@ -2,10 +2,12 @@ package com.patproject.test.battleship
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        appComponent.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
