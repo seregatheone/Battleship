672d939bf5706c6fa1f6f63d5c56fcc4ec21c7c4
seregatheone <sergey20030707@gmail.com>
2022-09-17 21:48:01 +0300

    placement stage and custom board elems

---
+++ app/src/main/java/com/patproject/test/battleship/ui/placement/PlacementStageFragment.kt
@@ -0,0 +1,10 @@
+package com.patproject.test.battleship.ui.placement
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.patproject.test.battleship.R
+import com.patproject.test.battleship.databinding.FragmentPlacementStageBinding
+
+class PlacementStageFragment : Fragment(R.layout.fragment_placement_stage) {
+    private val viewBinding by viewBinding(FragmentPlacementStageBinding::bind)
+}
---
+++ app/src/main/java/com/patproject/test/battleship/ui/singleplayer/SinglePlayerViewModel.kt
@@ -0,0 +1,6 @@
+package com.patproject.test.battleship.ui.singleplayer
+
+import androidx.lifecycle.ViewModel
+
+class SinglePlayerViewModel : ViewModel() {
+}
